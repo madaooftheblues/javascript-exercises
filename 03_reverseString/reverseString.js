20caf75ae2bdb2ddd54f165071187f784a340e70
madaooftheblues <azlanrasool@gmail.com>
2022-07-28 13:47:57 +0500

    Complete task 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let strArray = Array.from(string);
+  let reversed = [];
+  let j = 0;
+  for (let i = strArray.length; i >= 0; i--) {
+    reversed[j] = strArray[i];
+    j++;
+  }
+  return reversed.join("");
 };
 
 // Do not edit below this line
