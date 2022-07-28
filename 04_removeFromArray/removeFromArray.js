24ebe5936a589a0077ad2fccb4b3de557e446e23
madaooftheblues <azlanrasool@gmail.com>
2022-07-28 14:44:46 +0500

    Complete task 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  for (const arg of args) {
+    let index = array.indexOf(arg);
+    if (index != -1) array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
