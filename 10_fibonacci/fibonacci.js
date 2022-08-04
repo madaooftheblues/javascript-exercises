cbb91ca8bc80778404544ea915327148bd5413f1
madaooftheblues <azlanrasool@gmail.com>
2022-08-04 12:26:18 +0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  function fibIter(a, b, count) {
+    return count > n ? a : fibIter(b, a + b, count + 1);
+  }
 
+  return fibIter(0, 1, 1);
 };
 
 // Do not edit below this line
