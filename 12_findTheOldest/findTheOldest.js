abab74a194a3d657c9967b9d8258607a16f565d8
madaooftheblues <azlanrasool@gmail.com>
2022-08-04 13:23:38 +0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const oldSort = array.sort((a, b) => {
+    const ageA = a.yearOfDeath
+      ? a.yearOfDeath - a.yearOfBirth
+      : new Date().getFullYear() - a.yearOfBirth;
 
+    const ageB = b.yearOfDeath
+      ? b.yearOfDeath - b.yearOfBirth
+      : new Date().getFullYear() - b.yearOfBirth;
+
+    return ageB - ageA;
+  });
+
+  return oldSort[0];
 };
 
 // Do not edit below this line
