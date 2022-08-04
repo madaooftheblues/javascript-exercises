db65db94b0420ea3c51130d55814b6eeed8da806
madaooftheblues <azlanrasool@gmail.com>
2022-08-04 12:13:02 +0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphaNumeric = string.toLowerCase().replace(/[^a-z\d]/g, "");
 
+  return [...alphaNumeric].reverse().join("") === alphaNumeric;
 };
 
 // Do not edit below this line
